949bb5b2d27cc85a0d57196b09fd9fa3cf7b170c
Collin Stevens <git@collinstevens.com>
2026-03-20 03:09:35 -0700

    simplify event handling with EventUtil.ContinueOnAddOnLoaded

--- HideCharacterModelInCharacterInfo.lua
+++ HideCharacterModelInCharacterInfo.lua
@@ -1,10 +1,7 @@
-local HideCharacterModelInCharacterInfo = CreateFrame("Frame", "HideCharacterModelInCharacterInfo")
-HideCharacterModelInCharacterInfo:RegisterEvent("PLAYER_LOGIN")
-HideCharacterModelInCharacterInfo:SetScript("OnEvent", function(self)
+EventUtil.ContinueOnAddOnLoaded("Blizzard_UIPanels_Game", function()
     if CharacterModelScene then
         CharacterModelScene:HookScript("OnShow", function(self)
             self:Hide()
         end)
     end
-    self:UnregisterEvent("PLAYER_LOGIN")
 end)
